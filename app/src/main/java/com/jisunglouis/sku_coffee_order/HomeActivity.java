720ab32d27e9689831d3663bf9d3d6882c89d8d9
이원건 <wongun1850@gmail.com>
2024-06-08 16:46:05 +0900

    홈화면 XML,JAVA 1차 수정

--- app/src/main/java/com/jisunglouis/sku_coffee_order/HomeActivity.java
+++ app/src/main/java/com/jisunglouis/sku_coffee_order/HomeActivity.java
@@ -3,12 +3,30 @@ package com.jisunglouis.sku_coffee_order;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.ImageView;
 
 public class HomeActivity extends AppCompatActivity {
+
+    private ImageView home_menu;
+    private ImageView home_barcode;
+    private ImageView home_notification;
+    private ImageView home_order;
+    private ImageView home_payment;
+    private ImageView home_coupon;
+
+    private ImageView home_map;
+
+    private ImageView home_myinfo;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.home_activity);
+    }
+
+    public void home_menu(View v) {
+
     }
 }
