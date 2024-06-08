0f4e06c5860eb1d05a078799a18208768b9dc032
Jisung Chun <khkh9357@naver.com>
2024-06-09 00:27:24 +0900

    Jisung - OrderActivity 2개의 리사이클러뷰 구현

--- app/src/main/java/com/jisunglouis/sku_coffee_order/OrderActivity.java
+++ app/src/main/java/com/jisunglouis/sku_coffee_order/OrderActivity.java
@@ -4,28 +4,58 @@ import android.os.Bundle;
 import android.view.View;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.GridLayoutManager;
 import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 
+import com.jisunglouis.sku_coffee_order.OrderActivity_RecyclerViewAdapter;
+
 public class OrderActivity extends AppCompatActivity implements OrderActivity_RecyclerViewAdapter.ItemClickListener {
-    OrderActivity_RecyclerViewAdapter adapter;
+    OrderActivity_RecyclerViewAdapter category_adapter, list_adapter;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.order_activity);
 
-        String[] data = new String[100];
-        for(int i=1;i<=100;i++){
-            data[i-1]="friend #"+1;
-        }
-        RecyclerView recyclerView = findViewById(R.id.rview);
-        recyclerView.setLayoutManager(new LinearLayoutManager(this,RecyclerView.HORIZONTAL,false));
-        adapter = new OrderActivity_RecyclerViewAdapter(this,data);
-        adapter.setClickListener(this);
-        recyclerView.setAdapter(adapter);
-    }
-    @Override
-    public void onItemClick(View view, int position){
+        String[] data = {
+                "NEW",
+                "커피",
+                "콜드브루",
+                "논 커피",
+                "티/에이드",
+                "프라페/블렌디드",
+                "병음료"
+        };
 
+        String[] data2 = {
+                "Latte",
+                "Espresso",
+                "Americano",
+                "Cappuccino",
+                "Macchiato",
+                "Mocha",
+                "Affogato"
+        };
+
+        RecyclerView recyclerView1 = findViewById(R.id.rview); // 커피 카테고리
+        recyclerView1.setLayoutManager(new LinearLayoutManager(this, RecyclerView.HORIZONTAL, false));
+        category_adapter = new OrderActivity_RecyclerViewAdapter(this, data, OrderActivity_RecyclerViewAdapter.VIEW_TYPE_1);
+        category_adapter.setClickListener(this);
+        recyclerView1.setAdapter(category_adapter);
+
+        int columns = 3; // 커피 카테고리 별 커피 리스트
+        RecyclerView recyclerView2 = findViewById(R.id.rview2);
+        recyclerView2.setLayoutManager(new GridLayoutManager(this, columns));
+        list_adapter = new OrderActivity_RecyclerViewAdapter(this, data2, OrderActivity_RecyclerViewAdapter.VIEW_TYPE_2);
+        list_adapter.setClickListener(this);
+        recyclerView2.setAdapter(list_adapter);
+    }
+
+    @Override
+    public void onItemClick(View view, int position, int viewType) {
+        if (viewType == OrderActivity_RecyclerViewAdapter.VIEW_TYPE_1) {
+            category_adapter.setSelectedPosition(position);
+        }
     }
 }
--- app/src/main/java/com/jisunglouis/sku_coffee_order/OrderActivity_RecyclerViewAdapter.java
+++ app/src/main/java/com/jisunglouis/sku_coffee_order/OrderActivity_RecyclerViewAdapter.java
@@ -1,45 +1,74 @@
 package com.jisunglouis.sku_coffee_order;
 
 import android.content.Context;
-import android.text.Layout;
+import android.graphics.Paint;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.TextView;
 
-import androidx.annotation.NonNull;
 import androidx.recyclerview.widget.RecyclerView;
 
-public class OrderActivity_RecyclerViewAdapter extends RecyclerView.Adapter<OrderActivity_RecyclerViewAdapter.ViewHolder>{
+public class OrderActivity_RecyclerViewAdapter extends RecyclerView.Adapter<RecyclerView.ViewHolder> {
 
     private String[] mData;
     private LayoutInflater mInflater;
     private ItemClickListener mClickListener;
+    private int selectedPosition = -1;
+    public static final int VIEW_TYPE_1 = 1;
+    public static final int VIEW_TYPE_2 = 2;
+    private int viewType;
 
-    OrderActivity_RecyclerViewAdapter(Context context, String[] data){
+    // 생성자
+    public OrderActivity_RecyclerViewAdapter(Context context, String[] data, int viewType) {
         this.mInflater = LayoutInflater.from(context);
         this.mData = data;
+        this.viewType = viewType;
     }
 
     @Override
-    @NonNull
-    public ViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType){
-        View view = mInflater.inflate(R.layout.order_coffee_category,parent,false);
-        return new ViewHolder(view);
+    public int getItemViewType(int position) {
+        return viewType;
     }
 
     @Override
-    public void onBindViewHolder(@NonNull ViewHolder holder, int position){
-        holder.myTextView.setText(mData[position]);
+    public RecyclerView.ViewHolder onCreateViewHolder(ViewGroup parent, int viewType) {
+        View view;
+        if (viewType == VIEW_TYPE_1) {
+            view = mInflater.inflate(R.layout.order_coffee_category, parent, false);
+            return new CategoryViewHolder(view);
+        } else {
+            view = mInflater.inflate(R.layout.order_coffee_list, parent, false);
+            return new CoffeeViewHolder(view);
+        }
     }
-    public int getItemCount(){
+
+    @Override
+    public void onBindViewHolder(RecyclerView.ViewHolder holder, int position) {
+        String item = mData[position];
+        if (holder instanceof CategoryViewHolder) {
+            CategoryViewHolder categoryHolder = (CategoryViewHolder) holder;
+            categoryHolder.myTextView.setText(item);
+            if (position == selectedPosition) {
+                categoryHolder.myTextView.setPaintFlags(categoryHolder.myTextView.getPaintFlags() | Paint.UNDERLINE_TEXT_FLAG);
+            } else {
+                categoryHolder.myTextView.setPaintFlags(categoryHolder.myTextView.getPaintFlags() & (~Paint.UNDERLINE_TEXT_FLAG));
+            }
+        } else if (holder instanceof CoffeeViewHolder) {
+            CoffeeViewHolder coffeeHolder = (CoffeeViewHolder) holder;
+            coffeeHolder.myTextView.setText(item);
+        }
+    }
+
+    @Override
+    public int getItemCount() {
         return mData.length;
     }
 
-    public class ViewHolder extends RecyclerView.ViewHolder implements View.OnClickListener {
+    public class CategoryViewHolder extends RecyclerView.ViewHolder implements View.OnClickListener {
         TextView myTextView;
 
-        ViewHolder(View itemView) {
+        CategoryViewHolder(View itemView) {
             super(itemView);
             myTextView = itemView.findViewById(R.id.coffeeCategory);
             itemView.setOnClickListener(this);
@@ -47,16 +76,38 @@ public class OrderActivity_RecyclerViewAdapter extends RecyclerView.Adapter<Orde
 
         @Override
         public void onClick(View view) {
-            if (mClickListener != null) mClickListener.onItemClick(view, getAdapterPosition());
+            if (mClickListener != null) mClickListener.onItemClick(view, getAdapterPosition(), viewType);
         }
     }
-        String getItem(int id) {
-            return mData[id];
+
+    public class CoffeeViewHolder extends RecyclerView.ViewHolder implements View.OnClickListener {
+        TextView myTextView;
+
+        CoffeeViewHolder(View itemView) {
+            super(itemView);
+            myTextView = itemView.findViewById(R.id.coffeeList_coffeeName);
+            itemView.setOnClickListener(this);
         }
-        void setClickListener(ItemClickListener itemClickListener){
-            this.mClickListener = itemClickListener;
-        }
-        public interface ItemClickListener{
-            void onItemClick(View view, int position);
+
+        @Override
+        public void onClick(View view) {
+            if (mClickListener != null) mClickListener.onItemClick(view, getAdapterPosition(), viewType);
         }
     }
+
+    // 아이템 클릭 리스너를 설정하는 메서드
+    void setClickListener(ItemClickListener itemClickListener) {
+        this.mClickListener = itemClickListener;
+    }
+
+    // 선택된 위치를 설정하는 메서드 추가
+    public void setSelectedPosition(int position) {
+        selectedPosition = position;
+        notifyDataSetChanged();
+    }
+
+    // 아이템 클릭 리스너 인터페이스
+    public interface ItemClickListener {
+        void onItemClick(View view, int position, int viewType);
+    }
+}
